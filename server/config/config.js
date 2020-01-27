c39de63a8839d81eb0b305082d69feda758e8c1d
Neo <jose.vasquez@cist.cr>
2020-01-27 16:49:01 -0600

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,17 @@
 // Puerto
 // =======================================
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB = "mongodb+srv://admnode:Ez6r5RqvHOF01h12@nodejs-shk33.mongodb.net/cafe";
+}
+
+urlDB = "mongodb+srv://admnode:Ez6r5RqvHOF01h12@nodejs-shk33.mongodb.net/cafe";
+
+process.env.URL_DB = urlDB;
