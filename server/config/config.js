bf9cff12e742ec8bf0759a05c22f5cd044accbe4
Neo <jose.vasquez@cist.cr>
2020-01-27 17:25:51 -0600

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,9 +10,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://admnode:Ez6r5RqvHOF01h12@nodejs-shk33.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
-urlDB = "mongodb+srv://admnode:Ez6r5RqvHOF01h12@nodejs-shk33.mongodb.net/cafe";
+urlDB = process.env.MONGO_URI;
 
 process.env.URL_DB = urlDB;
